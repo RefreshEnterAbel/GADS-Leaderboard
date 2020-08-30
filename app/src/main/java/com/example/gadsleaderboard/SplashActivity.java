72e81e360315c4c99125fff5fad2f6e0cea6b7df
ABEL TADESSE <42779726+RefreshEnterAbel@users.noreply.github.com>
2020-08-30 22:35:36 +0300

    feat : initial commit

---
+++ app/src/main/java/com/example/gadsleaderboard/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.example.gadsleaderboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+    }
+}
+
