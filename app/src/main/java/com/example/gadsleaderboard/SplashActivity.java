195b132ea0ac1cb8bd2daf7a489d3ca0e40e7543
ABEL TADESSE <42779726+RefreshEnterAbel@users.noreply.github.com>
2020-08-30 23:36:50 +0300

    feat:  work with a custom Handler for SplashActivity

--- app/src/main/java/com/example/gadsleaderboard/SplashActivity.java
+++ app/src/main/java/com/example/gadsleaderboard/SplashActivity.java
@@ -1,15 +1,44 @@
 package com.example.gadsleaderboard;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashActivity extends AppCompatActivity {
+
+    private final static int MSG_CONTINUE = 1234;
+    private final static long DELAY = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        mHandler.sendEmptyMessageDelayed(MSG_CONTINUE, DELAY);
+
     }
+
+    @Override
+    protected void onDestroy()
+    {
+        mHandler.removeMessages( MSG_CONTINUE );
+        super.onDestroy();
+    }
+
+    private void _continue()
+    {
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
+    }
+
+    private final Handler mHandler = new Handler(){
+        public void handleMessage(android.os.Message msg){
+            if (msg.what == MSG_CONTINUE) {
+                _continue();
+            }
+        }
+    };
 }
 
