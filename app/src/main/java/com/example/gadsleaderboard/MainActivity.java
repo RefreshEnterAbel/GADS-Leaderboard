9a3dd694a83f3fc5a54e121aa5ac933348fd6e59
ABEL TADESSE <42779726+RefreshEnterAbel@users.noreply.github.com>
2020-09-07 17:03:19 +0300

    feat: customize splash layout file on order to get correct view

--- app/src/main/java/com/example/gadsleaderboard/MainActivity.java
+++ app/src/main/java/com/example/gadsleaderboard/MainActivity.java
@@ -12,6 +12,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toolbar;
 
 import com.example.gadsleaderboard.ui.main.SectionsPagerAdapter;
 
@@ -26,14 +27,6 @@ public class MainActivity extends AppCompatActivity {
         viewPager.setAdapter(sectionsPagerAdapter);
         TabLayout tabs = findViewById(R.id.tabs);
         tabs.setupWithViewPager(viewPager);
-        FloatingActionButton fab = findViewById(R.id.fab);
 
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 }
