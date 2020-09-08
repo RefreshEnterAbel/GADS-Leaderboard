f776609981765fcb195764bd79b2ceba557c5a21
ABEL TADESSE <42779726+RefreshEnterAbel@users.noreply.github.com>
2020-09-09 02:58:51 +0300

    feat : pass intent to submit project activity

--- app/src/main/java/com/example/gadsleaderboard/MainActivity.java
+++ app/src/main/java/com/example/gadsleaderboard/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.gadsleaderboard;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -47,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                startActivity(new Intent(v.getContext(),ProjectSubmissionActivity.class));
             }
         });
 
