b63341c8e266bd5e21e67eb0b24fa6214806b6be
ABEL TADESSE <42779726+RefreshEnterAbel@users.noreply.github.com>
2020-09-09 00:11:53 +0300

    feat : add Project Submission Activity

---
+++ app/src/main/java/com/example/gadsleaderboard/ProjectSubmissionActivity.java
@@ -0,0 +1,14 @@
+package com.example.gadsleaderboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProjectSubmissionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_project_submission);
+    }
+}
