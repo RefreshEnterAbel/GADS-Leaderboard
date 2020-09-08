09ba343f062bfd49783a44f729db0a04993b94b0
ABEL TADESSE <42779726+RefreshEnterAbel@users.noreply.github.com>
2020-09-09 02:51:49 +0300

    feat : toolbar customize effect

--- app/src/main/java/com/example/gadsleaderboard/ProjectSubmissionActivity.java
+++ app/src/main/java/com/example/gadsleaderboard/ProjectSubmissionActivity.java
@@ -1,6 +1,8 @@
 package com.example.gadsleaderboard;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -10,5 +12,11 @@ public class ProjectSubmissionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_project_submission);
+        Toolbar toolbar = findViewById(R.id.toolbar2);
+        setSupportActionBar(toolbar);
+        ActionBar supportActionBar = getSupportActionBar();
+        supportActionBar.setTitle(null);
+        supportActionBar.setDisplayHomeAsUpEnabled(true);
+
     }
 }
